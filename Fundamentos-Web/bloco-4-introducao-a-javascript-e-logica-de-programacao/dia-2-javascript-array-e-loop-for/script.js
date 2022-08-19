a578b66da542f200bc9986ef626044405e959b96
Leo Pestille <leonardopestille@gmail.com>
2022-08-19 18:02:07 -0400

    calculando media aritmetica

--- Fundamentos-Web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ Fundamentos-Web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -3,8 +3,11 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let sum = 0;
 
+let media = numbers.length - 1;
+
 for (let i = 0; i < numbers.length; i++) {
   sum += numbers[i];
 }
+let result = sum / numbers.length;
 
-console.log(sum);
+console.log(result);
