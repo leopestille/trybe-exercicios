dc796a2a4c4982716f7e4d9a8424abbe8683ae3c
Leo Pestille <leonardopestille@gmail.com>
2022-08-19 18:15:26 -0400

    verificando maior valor do array

--- Fundamentos-Web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ Fundamentos-Web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -1,17 +1,10 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maxValue = 0;
 
-let sum = 0;
-
-let media = numbers.length - 1;
-
-for (let i = 0; i < numbers.length; i++) {
-  sum += numbers[i];
+for (let i = 0; i < numbers.length; i+= 1) {
+    if (numbers[i] > maxValue) {
+        maxValue = numbers[i];
+    }
 }
-let result = sum / numbers.length;
-
-if(result > 20) {
-  console.log("The result is higher than 20");
-}else{
-  console.log("The result is lower or equal than 20");
-}
+console.log(maxValue);
