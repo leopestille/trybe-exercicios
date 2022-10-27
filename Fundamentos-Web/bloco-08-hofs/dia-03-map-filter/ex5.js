e7f50772198334740c6264fd1dceca6becc8d46d
Leo Pestille <leonardopestille@gmail.com>
2022-10-27 12:38:54 -0400

    function fantasyOrScienceFictionAuthors step 2 and 3

--- Fundamentos-Web/bloco-08-hofs/dia-03-map-filter/ex5.js
+++ Fundamentos-Web/bloco-08-hofs/dia-03-map-filter/ex5.js
@@ -63,4 +63,7 @@ const books = [
 
 function fantasyOrScienceFictionAuthors() {
     const wantedGenres = ['Fantasia', 'Ficção Científica'];
+    return books.filter((book) => wantedGenres.includes(book.genre)).map((book) => book.author.name).sort();
 }
+
+console.log(fantasyOrScienceFictionAuthors());
