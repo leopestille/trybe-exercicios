102b0374e70878aac65fa25eb8c19589f962eef6
Leo Pestille <leonardopestille@gmail.com>
2022-10-27 12:44:08 -0400

    function oldBooks() step2

--- Fundamentos-Web/bloco-08-hofs/dia-03-map-filter/ex6.js
+++ Fundamentos-Web/bloco-08-hofs/dia-03-map-filter/ex6.js
@@ -63,5 +63,7 @@ const books = [
 
 function oldBooks(){
     const currentYear = new Date().getFullYear();
-    return books.filter((book) => currentYear - book.releaseYear > 60)
+    return books.filter((book) => currentYear - book.releaseYear > 60).map((book) => book.name);
 }
+
+console.log(oldBooks());
