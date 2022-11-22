addc1c419f2a5cfad0f1f646578dcf503ea63e08
Leo Pestille <leonardopestille@gmail.com>
2022-11-22 17:21:13 -0400

    criando classe Sobre

---
+++ Front-End/React/minhas-habilidades/src/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Sobre extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    { " Leonardo Pestille Leão " }
+                </h1>
+                <p>
+                    { " Estudante de Desenvolvimento Web " }
+                </p>
+            </div>
+        );
+    }
+}
+
+export default Sobre;
