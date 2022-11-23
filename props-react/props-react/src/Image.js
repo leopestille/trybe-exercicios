1c8a6bfcd77fccbdd0d7322e45589779079282c8
Leo Pestille <leonardopestille@gmail.com>
2022-11-23 14:37:37 -0400

    criando Image.js

---
+++ props-react/props-react/src/Image.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+class Image extends React.Component {
+    render() {
+        const { source, alternativeText } = this.props;
+        return <img src={ source } alt={ alternativeText }></img>;
+    }
+}
+
+export default Image;
