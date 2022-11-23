586808fb7552199af7d4578b2f8c44ebdaf282ee
Leo Pestille <leonardopestille@gmail.com>
2022-11-23 14:46:23 -0400

    transformando em componente de classe

--- props-react/props-react/src/App.js
+++ props-react/props-react/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import staringCat from './staringCat.jpg';
 import Image from './Image';
 
-function App() {
-  const URL = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
-  return (
-    <main>
-      <Image source={URL} alternativeText="Cute cat staring" />
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <Image source={staringCat} alternativeText="Cute cat staring" />
+      </main>
+    );
+  }
 }
 
 export default App;
